626032152fa995892d23365b3090df19d312e338
Thayna Thauany <thayna@shockit.com.br>
2025-08-28 17:14:31 -0300

    feat: pagina de login finalizada

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,23 +6,28 @@ export default function Login() {
   const t = useTranslations("Login");
   return (
     <>
-      <div className="flex min-h-screen flex-col lg:flex-row">
+      <div className="flex min-h-full">
         <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
           <div className="mx-auto w-full max-w-sm lg:w-96">
             <div>
               <img
                 alt="Your Company"
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-10 w-auto"
+                src="/images/logos/dinerolatam.jpg"
+                className="h-10 w-auto dark:hidden"
               />
-              <h2 className="mt-8 text-2xl/9 font-bold tracking-tight text-gray-900">
+              <img
+                alt="Your Company"
+                src="/images/logos/dinerolatam.jpg"
+                className="h-10 w-auto not-dark:hidden"
+              />
+              <h2 className="mt-8 text-2xl/9 font-bold tracking-tight text-black dark:text-white">
                 {t("title")}
               </h2>
-              <p className="mt-2 text-sm/6 text-gray-500">
+              <p className="mt-2 text-sm/6 text-primary dark:text-primary">
                 {t("subtitle")}{" "}
                 <a
                   href="#"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  className="font-semibold text-secondary"
                 >
                   {t("startTrial")}
                 </a>
@@ -33,10 +38,7 @@ export default function Login() {
               <div>
                 <form action="#" method="POST" className="space-y-6">
                   <div>
-                    <label
-                      htmlFor="email"
-                      className="block text-sm/6 font-medium text-gray-900"
-                    >
+                    <label htmlFor="email" className="block text-sm/6 font-medium text-primary dark:text-primary">
                       {t("email")}
                     </label>
                     <div className="mt-2">
@@ -46,16 +48,13 @@ export default function Login() {
                         type="email"
                         required
                         autoComplete="email"
-                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-primary outline-1 -outline-offset-1 outline-gray-300 placeholder:text-primary focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-primary dark:focus:outline-indigo-500"
                       />
                     </div>
                   </div>
 
                   <div>
-                    <label
-                      htmlFor="password"
-                      className="block text-sm/6 font-medium text-gray-900"
-                    >
+                    <label htmlFor="password" className="block text-sm/6 font-medium text-primary dark:text-primary">
                       {t("password")}
                     </label>
                     <div className="mt-2">
@@ -65,7 +64,7 @@ export default function Login() {
                         type="password"
                         required
                         autoComplete="current-password"
-                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-primary outline-1 -outline-offset-1 outline-gray-300 placeholder:text-primary focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-primary dark:focus:outline-indigo-500"
                       />
                     </div>
                   </div>
@@ -78,7 +77,7 @@ export default function Login() {
                             id="remember-me"
                             name="remember-me"
                             type="checkbox"
-                            className="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
+                            className="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 dark:border-white/10 dark:bg-white/5 dark:checked:border-indigo-500 dark:checked:bg-indigo-500 dark:indeterminate:border-indigo-500 dark:indeterminate:bg-indigo-500 dark:focus-visible:outline-indigo-500 forced-colors:appearance-auto"
                           />
                           <svg
                             fill="none"
@@ -102,10 +101,7 @@ export default function Login() {
                           </svg>
                         </div>
                       </div>
-                      <label
-                        htmlFor="remember-me"
-                        className="block text-sm/6 text-gray-900"
-                      >
+                      <label htmlFor="remember-me" className="block text-sm/6 text-primary dark:text-primary">
                         {t("rememberMe")}
                       </label>
                     </div>
@@ -113,7 +109,7 @@ export default function Login() {
                     <div className="text-sm/6">
                       <a
                         href="#"
-                        className="font-semibold text-indigo-600 hover:text-indigo-500"
+                        className="font-semibold text-secondary"
                       >
                         {t("forgotPassword")}
                       </a>
@@ -123,7 +119,7 @@ export default function Login() {
                   <div>
                     <button
                       type="submit"
-                      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      className="flex w-full justify-center rounded-md bg-secondary px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs  focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:shadow-none dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
                     >
                       {t("signIn")}
                     </button>
@@ -133,14 +129,11 @@ export default function Login() {
 
               <div className="mt-10">
                 <div className="relative">
-                  <div
-                    aria-hidden="true"
-                    className="absolute inset-0 flex items-center"
-                  >
-                    <div className="w-full border-t border-gray-200" />
+                  <div aria-hidden="true" className="absolute inset-0 flex items-center">
+                    <div className="w-full border-t border-gray-200 dark:border-gray-700" />
                   </div>
                   <div className="relative flex justify-center text-sm/6 font-medium">
-                    <span className="bg-white px-6 text-gray-900">
+                    <span className="bg-white px-6 text-primary dark:bg-gray-900 dark:text-primary">
                       {t("orContinueWith")}
                     </span>
                   </div>
@@ -149,13 +142,9 @@ export default function Login() {
                 <div className="mt-6 grid grid-cols-2 gap-4">
                   <a
                     href="#"
-                    className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus-visible:ring-transparent"
+                    className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-primary shadow-xs inset-ring inset-ring-gray-300 hover:bg-gray-50 focus-visible:inset-ring-transparent dark:bg-white/10 dark:text-white dark:shadow-none dark:inset-ring-white/5 dark:hover:bg-white/20"
                   >
-                    <svg
-                      viewBox="0 0 24 24"
-                      aria-hidden="true"
-                      className="h-5 w-5"
-                    >
+                    <svg viewBox="0 0 24 24" aria-hidden="true" className="h-5 w-5">
                       <path
                         d="M12.0003 4.75C13.7703 4.75 15.3553 5.36002 16.6053 6.54998L20.0303 3.125C17.9502 1.19 15.2353 0 12.0003 0C7.31028 0 3.25527 2.69 1.28027 6.60998L5.27028 9.70498C6.21525 6.86002 8.87028 4.75 12.0003 4.75Z"
                         fill="#EA4335"
@@ -173,20 +162,18 @@ export default function Login() {
                         fill="#34A853"
                       />
                     </svg>
-                    <span className="text-sm/6 font-semibold">
-                      {t("google")}
-                    </span>
+                    <span className="text-sm/6 font-semibold">    {t("google")}</span>
                   </a>
 
                   <a
                     href="#"
-                    className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus-visible:ring-transparent"
+                    className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-primary shadow-xs inset-ring inset-ring-gray-300 hover:bg-gray-50 focus-visible:inset-ring-transparent dark:bg-white/10 dark:text-white dark:shadow-none dark:inset-ring-white/5 dark:hover:bg-white/20"
                   >
                     <svg
                       fill="currentColor"
                       viewBox="0 0 20 20"
                       aria-hidden="true"
-                      className="size-5 fill-[#24292F]"
+                      className="size-5 fill-[#24292F] dark:fill-white"
                     >
                       <path
                         d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
@@ -194,20 +181,18 @@ export default function Login() {
                         fillRule="evenodd"
                       />
                     </svg>
-                    <span className="text-sm/6 font-semibold">
-                      {t("github")}
-                    </span>
+                    <span className="text-sm/6 font-semibold">     {t("github")}</span>
                   </a>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="hidden lg:block lg:w-1/2 relative">
+        <div className="relative hidden w-0 flex-1 lg:block min-h-screen">
           <img
             alt=""
             src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
-            className="absolute inset-0 w-full h-full object-cover"
+            className="absolute inset-0 size-full object-cover"
           />
         </div>
       </div>
